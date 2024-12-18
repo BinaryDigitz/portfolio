917805274886195298a6adc6efa35254b183041c
Binary Digitz <borisndoh@gmail.com>
2024-12-18 20:35:15 +0100

    initial comit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className=" h-screen text-center pt-[10%]">
+      <h1 className=" text-3xl md:text-4xl lg:text-7xl text-green-500">Coming soon </h1>
+      <p className="pt-4">Email: borisndoh@gmail.com</p>
+      <p className="text-xl">CAM: +237 653775159</p>
+      <p className="text-xl">CY: +357 96821885</p>
+    </div>
+  )
+}
+
+export default App
